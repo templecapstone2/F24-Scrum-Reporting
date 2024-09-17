15f092f1de1fd10e68c7fce87e8427d2f8cd0b67
Jacob <jacobzall@gmail.com>
2024-09-16 20:26:51 -0400

    first commit - login and register page styling update

---
+++ Capstone-master/Capstone/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using Capstone.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Capstone.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Aggregate()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult Report()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
