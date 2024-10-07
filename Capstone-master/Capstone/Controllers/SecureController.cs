e2677e65cd56c29154a44ba5188f0db5293d3446
tun30817 <tun30817@temple.edu>
2024-10-07 19:30:07 -0400

    Added secure folder and made progress with shibboleth

---
+++ Capstone-master/Capstone/Controllers/SecureController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Controllers
+{
+    public class SecureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit()
+        {
+            bool loginSuccessful = true;
+
+            if (loginSuccessful)
+            {
+                return RedirectToAction("ProfessorHome", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Login failed. Please try again.";
+                return View("~/Secure/Login");
+            }
+        }
+    }
+}
--- Capstone-master/Capstone/Program.cs
+++ Capstone-master/Capstone/Program.cs
@@ -2,22 +2,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
-
+builder.Configuration.GetConnectionString("Connection_Database");
+app.UsePathBase("/cis4396-f05");
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
+string routePrefix = app.Environment.IsProduction() ? "cis4396-f05" : "";
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=User}/{action=Login}/{id?}");
+  name: "default",
+pattern: "{controller=Login}/{action=Login}/{id?}");
 
 app.Run();
+
