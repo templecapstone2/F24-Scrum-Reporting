17af7d96a1cc676ee0c815e12fa49db79e0cbbc0
tul20017 <tul20017@TUStemwin10-2>
2024-10-21 00:48:26 -0400

    Added viewmodel for student management page and my TUID to the securecontroller for an additional student in the db

---
+++ Capstone-master/Capstone/ViewModels/StudentManagementModel.cs
@@ -0,0 +1,38 @@
+﻿using Capstone.Models;
+
+namespace Capstone.ViewModels
+{
+    public class StudentManagementModel
+    {
+        private List<User> students;
+        private List<Team> teams;
+        private List<TeamUser> teamUsers;
+
+        public StudentManagementModel() { }
+
+        public StudentManagementModel(List<User> students, List<Team> teams, List<TeamUser> teamUsers)
+        {
+            this.students = students;
+            this.teams = teams;
+            this.teamUsers = teamUsers;
+        }
+
+        public List<User> Students
+        {
+            get { return students; }
+            set { this.students = value; }
+        }
+
+        public List<Team> Teams
+        {
+            get { return teams; }
+            set { this.teams = value; }
+        }
+
+        public List<TeamUser> TeamUsers
+        {
+            get { return teamUsers; }
+            set { this.teamUsers = value; }
+        }
+    }
+}
